a431c9b74cb955a1e2c9534264edef9fe77e7d07
Nya0 <Nya0>
2024-02-17 02:43:50 +0300

    overhauld1

--- src/main.lua
+++ src/main.lua
@@ -1,11 +1,9 @@
-package.path = package.path .. ';/usr/local/?.lua'
-
 local Lummander = require("lummander")
 
 local cli = Lummander.new{
     title = "Inlyzer", 
     tag = "inlyzer", 
-    description = "My App description",
+    description = "Intrusion Detection Analyzer blah blah",
     version = "0.1",
     author = "Cyberzone",
     theme = "black", 
@@ -14,21 +12,23 @@ local cli = Lummander.new{
 
 -- Add commands
 local commands = {
-    dashboard = cli:command("dashboard [state]", "Starts the dashboard"),
+    dashboard = cli:command("dashboard <state>", "Starts the dashboard"),
     sum = cli:command("sum <value1> <value2>", "Sum 2 values")
 }
 
 commands.dashboard:action(function(parsed, command, app)
-    print("You activated `mycmd` command")
+    if parsed.state == "on" then
+
+    elseif parsed.state == "off" then
+
+    end
 end)
 
 
 commands.sum:option("option1","o","Option1 description",nil,"normal","option_default_value")
-commands.sum:option("option2","p","Option2 description",nil,"normal","option2_default_value")
 commands.sum:action(function(parsed, command, app)
     print("".. parsed.value1.. "+"..parsed.value2.." = " ..tostring(tonumber(parsed.value1) + tonumber(parsed.value2)))
 end)
 
 cli:parse(arg)
 
--- d
